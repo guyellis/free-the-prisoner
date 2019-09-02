b0854c99c87e896d91bf0e152d46a12cfa67ceec
Guy Ellis <guy.ellis@walmartlabs.com>
2019-09-02 12:19:57 -0700

    Add a script to generate cryptograms

---
+++ scripts/crypto-generator.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const keyMap = {};
+
+const asciiA = 'A'.charCodeAt(0);
+const asciiZ = 'Z'.charCodeAt(0);
+const randomUpperChar = () => {
+  const random = Math.random() * (asciiZ - asciiA) + asciiA;
+  return String.fromCharCode(random);
+};
+
+/**
+ * 
+ * @param {string} char - the character to encrypt
+ * @returns {string} - encrypted char
+ */
+const encryptChar = (char) => {
+  if (!char.match(/^[A-Z]*$/)) {
+    return char;
+  }
+  if (keyMap[char]) {
+    return keyMap[char];
+  }
+  let rndAlpha;
+  do {
+    rndAlpha = randomUpperChar();
+  } while (rndAlpha !== char && !keyMap[rndAlpha]);
+
+  keyMap[char] = rndAlpha;
+  return keyMap[char];
+}
+
+const encryptLine = (line) => line
+  .split('')
+  .map(encryptChar)
+  .join('');
+
+const generateOutput = (contents) => {
+  const inLines = contents.split('\n');
+  const outLines = inLines.map(encryptLine);
+  const outContentLines = outLines.reduce((acc, outLine) => {
+    acc.push(outLine.split('').map((char) => char.match(/^[A-Z]*$/) ? '_' : char).join(''));
+    acc.push(outLine);
+    return acc;
+  }, []);
+  return outContentLines.join('\n');
+};
+
+const inFile = path.join(__dirname, '../puzzles/mountain-lion-cub/cryptograms.txt')
+const outFile = path.join(__dirname, '../puzzles/mountain-lion-cub/printables/cryptograms.txt')
+
+const fileContent = fs.readFileSync(inFile).toString().toUpperCase();
+const encryptedContents = generateOutput(fileContent);
+fs.writeFileSync(outFile, encryptedContents);
