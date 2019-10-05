94df1355c0ba84cae24474c3520e9ac6fafd1ee1
Guy Ellis <guy.ellis@walmartlabs.com>
2019-10-05 12:00:22 -0700

    Add some mocking for tests

--- test/lib/crypto-gen.test.ts
+++ test/lib/crypto-gen.test.ts
@@ -1,4 +1,9 @@
 import { createEncryptedFile } from '../../lib/crypto-gen';
+import fs from 'fs';
+
+jest.mock('fs');
+fs.readFileSync = jest.fn();
+fs.writeFileSync = jest.fn();
 
 describe('crypt-gen', () => {
     it('works', () => {
